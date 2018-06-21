793d4ec4472c416a790fae26fbdc2e284b478f46
Alejandro Guayara <ca.guayara93@gmail.com>
2018-06-21 16:55:09 -0500

    TileSet assets upload

---
+++ assets/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="background" tilewidth="16" tileheight="16" tilecount="616" columns="22">
+ <image source="background.png" width="352" height="448"/>
+</tileset>
---
+++ assets/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ground" tilewidth="16" tileheight="16" tilecount="21" columns="7">
+ <image source="ground.png" trans="ffffff" width="112" height="48"/>
+</tileset>
