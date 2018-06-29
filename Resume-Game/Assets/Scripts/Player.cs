b47585b26a901f40287d82d73d24505d145cc02c
Alejandro Guayara <ca.guayara93@gmail.com>
2018-06-29 01:29:32 -0500

    Player Animator Reset & Player Script

---
+++ Resume-Game/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float horizontalVelocity;
+	public float jumpVelocity;
+	public float fallMultiplier = 2.5f;
+	private Animator animator;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Awake () {
+		animator = GetComponent<Animator>( );
+		rb = GetComponent<Rigidbody2D>( );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalMove = Input.GetAxis( "Horizontal" ) * horizontalVelocity * Time.deltaTime;
+		transform.Translate( horizontalMove, 0, 0 );
+
+		if( Input.GetButtonDown( "Horizontal" ) ) {
+			animator.SetBool( "playerRun", true );
+		}
+		else if( Input.GetButtonUp( "Horizontal" ) ) {
+			animator.SetBool( "playerRun", false );
+		}
+
+		if( Input.GetButtonDown( "Jump" ) ) {
+			rb.velocity = Vector2.up * jumpVelocity;
+			animator.SetTrigger( "playerJump" );
+		}
+
+		if( rb.velocity.y < 0 ) {
+			rb.velocity += Vector2.up * Physics2D.gravity.y * ( fallMultiplier - 1 ) * Time.deltaTime;
+			animator.SetTrigger( "playerFall" );
+		}
+	}
+}
